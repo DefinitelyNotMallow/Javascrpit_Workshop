3120abe2db7738a625bbcc2f1cd53d5cdd233aa8
jujulegeek <julian.lambert@epitech.eu>
2023-04-26 17:20:28 +0200

    Battery

--- Battery.js
+++ Battery.js
@@ -1,5 +1,14 @@
 function battery(list) 
 {
+    let found = false;
+    list.forEach(element => {
+        if (element !== 1){
+            console.log("Found !")
+            found = true;
+        }
+    });
+    if (!found)
+        console.log("ALL THESE BATTERIES ARE THE SAME !!!!!!")
 }
 
 module.exports = battery;
